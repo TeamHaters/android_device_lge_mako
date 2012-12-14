9139f7f9f5ad6ad15456204aae173eccb004a0db
Chris Phelps <cphelps76@cphelps76-HP-Pavilion-dv7.(none)>
2012-12-14 02:40:17 -0600

    build a prebuilt kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,12 +30,6 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=mako lpj=67677
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01600000
 
-# Build the kernel
-# Comment this to use CM kernel source
-TARGET_KERNEL_SOURCE := kernel/lge/mako-franco
-
-TARGET_KERNEL_CONFIG := mako_defconfig
-
 BOARD_USES_ALSA_AUDIO:= true
 BOARD_USES_FLUENCE_INCALL := true
 BOARD_USES_SEPERATED_AUDIO_INPUT := true
